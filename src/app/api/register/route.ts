b0bd693da73605b82b9b39f14989675d7f2b6bd5
Gabriel K <gab270b@gmail.com>
2025-06-04 15:14:09 +0200

    fix cloudinary now upload files only if we're not in test

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -5,7 +5,7 @@ import { RegisterDataWithUrls } from '@/lib/types/types'
 import { cloudinaryService } from '@/lib/cloudinary/cloudinary'
 import cuid from 'cuid'
 
-const shouldUpload = process.env.DISABLE_CLOUDINARY !== 'true'
+const disableUpload = process.env.NODE_ENV === 'test'
 const fallbackAvatarUrl = 'https://media.discordapp.net/attachments/1357283554769502361/1376852929633325096/image.png?ex=68401037&is=683ebeb7&hm=ce23ac84db91474439ac5b722376061c96ca64c6c15f03af6ae70665cea3aad8&=&format=webp&quality=lossless'
 const fallbackCoverUrl = 'https://cdn.discordapp.com/attachments/1357283554769502361/1376847788372918342/konekt-high-resolution-logo.png?ex=68400b6d&is=683eb9ed&hm=74cdd867140f37dc46cc82122413067510c5a262a757fa0e96d53ab277427008&'
 export async function POST(req: NextRequest): Promise<NextResponse> {
@@ -26,7 +26,7 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
         const avatarFile = formData.get('avatar') as File | null
         const coverFile = formData.get('cover') as File | null
 
-        if (shouldUpload && avatarFile) {
+        if (!disableUpload && avatarFile) {
             const avatarBuffer = Buffer.from(await avatarFile.arrayBuffer())
             const uploadResult = await cloudinaryService.uploadImage(avatarBuffer, { format: userId, filename: avatarFile.name })
             data.avatar = uploadResult?.secure_url ?? fallbackAvatarUrl
@@ -34,7 +34,7 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
             data.avatar = fallbackAvatarUrl
         }
 
-        if (shouldUpload && coverFile) {
+        if (!disableUpload && coverFile) {
             const coverBuffer = Buffer.from(await coverFile.arrayBuffer())
             const uploadResult = await cloudinaryService.uploadImage(coverBuffer, { format: userId, filename: coverFile.name })
             data.cover = uploadResult?.secure_url ?? fallbackCoverUrl
