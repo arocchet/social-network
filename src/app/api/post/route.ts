29bcfe685614d5cea5950bab76f43dbf78a82e41
Adrien roc <adrieen.rocchetti@gmail.com>
2025-06-12 11:56:40 +0200

    Route /api/post
    - Récupération du body de la requête
    - Création du schéma zod + Vérification des données
    - Création du post en DB et renvoie de la réponse

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -1,8 +1,37 @@
-export function POST(request: Request) {
-  return new Response("Hello from POST route", {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+import { z } from "zod";
+
+const PostSchema = z.strictObject({
+  content: z.string().min(1, "Content is required"),
+  img: z.string().url("Image must be a valid URL"),
+});
+
+export async function POST(req: Request) {
+
+  // Récupération des données du corps de la requête
+  const { data } = await req.json()
+
+  // Validation des données avec Zod
+  const parseData = PostSchema.safeParse(data);
+
+  console.log("Parsed Data:", parseData);
+
+  // Vérification si la validation a échoué
+  if (!parseData.success) {
+    return NextResponse.json({ message: parseData.error.errors.map(err => err.message).join(", ") }, { status: 400 });
+  }
+
+  // Si la validation réussit, création de post
+  const res = await db.post.create({
+    data: {
+      message: parseData.data.content,
+      image: parseData.data.img,
+      visibility: "PRIVATE",
+      userId: "cmbqn7ofd00006dq8u89tefjd",
+    }
+  })
+
+  return NextResponse.json({ message: res, }, { status: 200 })
 }
